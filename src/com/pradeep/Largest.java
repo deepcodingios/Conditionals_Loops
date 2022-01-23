4f319f43d2b29ae9cc00f554e5f46a5c54d1d9ba
PradeepReddy <deepu462@gmail.com>
2022-01-23 20:16:28 +0530

    Find Largest Number

--- src/com/pradeep/Largest.java
+++ src/com/pradeep/Largest.java
@@ -1,4 +1,23 @@
 package com.pradeep;
 
+import java.util.Scanner;
+
 public class Largest {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number_1 = input.nextInt();
+        int number_2 = input.nextInt();
+        int number_3 = input.nextInt();
+
+        int max = number_1;
+        if (number_2 > number_1){
+            max = number_2;
+        }
+        if (number_3 > max) {
+            max = number_3;
+        }
+        System.out.println(max);
+    }
 }
